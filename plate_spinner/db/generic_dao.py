ee775122d5205809265cbb5b6818e61f52bc16b2
tsutsumi <tsutsumitaiga@gmail.com>
2019-12-15 17:23:29 +0900

    不要行の削除。

--- plate_spinner/db/generic_dao.py
+++ plate_spinner/db/generic_dao.py
@@ -2,26 +2,23 @@ class GenericDao(object):
     def session(self):
         pass
 
+    def check_mode(self):
+        pass
 
     def store_runnning(self):
         pass
 
-
     def remove_runnning(self):
         pass
-
 
     def build_dequeue_query(self, config, specified_jobnames=[], sharding_keys=[]):
         pass
 
-
     def store_taken_at(self):
         pass
 
-
     def store_finished_at(self):
         pass
-
 
     def check_killswitch(self):
         pass
