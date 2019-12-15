3827b0f9d4f2276c1eb2921ba8a943496a66754f
tsutsumi <tsutsumitaiga@gmail.com>
2019-12-15 17:23:05 +0900

    使用モデルを列記。

--- plate_spinner/db/__init__.py
+++ plate_spinner/db/__init__.py
@@ -3,4 +3,9 @@ from .generic_dao import GenericDao
 from .mysql import MySQL
 from .base import Base
 from .jobqueue_sun import JobQueueSun
+from .job_taken_sun import JobTakenSun
+from .jobqueue_moon import JobQueueMoon
+from .job_taken_moon import JobTakenMoon
 from .running import Running
+from .mode import Mode
+
